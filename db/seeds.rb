2b94535b5719dce8552e0dcf8919722f17217722
julie collazos <juliecollazos@hotmail.com>
2020-08-13 12:05:30 +0200

    model cocktails

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+# require 'open-uri'
+# 
+# def ingredient
+# response = open("https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list#{:ingredient}")
+# json = JSON.parse(response.read)
+# return json['found']
+# end
+
+Ingredient.create(name: "lemon")
+Ingredient.create(name: "ice")
+Ingredient.create(name: "mint leaves")
+Ingredient.create(name: "rhum")
+Ingredient.create(name: "concumber")
+Ingredient.create(name: "gin")
+Ingredient.create(name: "tonic")
+
+Cocktail.create(name:"mojito")
+Cocktail.create(name: "gin tonic")
+Cocktail.create(name: "concumber folie")
